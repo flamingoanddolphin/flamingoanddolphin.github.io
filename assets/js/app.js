2e2bf2918edf19d37e64c3364a7091daef3f27eb
xiaoliang zhang <dr.xzhang@hotmail.com>
2025-12-30 14:29:58 +0000

    update

--- assets/js/app.js
+++
@@ -1,7 +0,0 @@
-﻿//Scroll Init
-$('.page-scroll').on('click', function (e) {
-    e.preventDefault();
-    var $anchor = $(this);
-    var id = $anchor[0].hash;
-    $('html, body').stop().animate({ scrollTop: $(id).offset().top }, 1500, 'easeInOutExpo');
-});
